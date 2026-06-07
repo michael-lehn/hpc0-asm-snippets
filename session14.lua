e7f1b6bd84bf8817a2279c2ceff39aed93953c13
Michael Lehn <michael.lehn@uni-ulm.de>
2026-06-07 14:22:42 +0200

    cleanup

--- session14.lua
+++ session14.lua
@@ -3,7 +3,6 @@ local function asm_define_function()
 	if name == "" then
 		return
 	end
-
 	local lines = {
 		name .. ":",
 		"\tsubq 8, %SP, %SP",
@@ -16,12 +15,8 @@ local function asm_define_function()
 		"\taddq 8, %SP, %SP",
 		"\tret %2",
 	}
-
 	local row = vim.api.nvim_win_get_cursor(0)[1]
-
 	vim.api.nvim_buf_set_lines(0, row - 1, row - 1, false, lines)
-
-	-- Cursor in die Leerzeile zwischen begin/end setzen
 	vim.api.nvim_win_set_cursor(0, { row + 4, 4 })
 end
 
@@ -30,31 +25,22 @@ local function asm_call_function()
 	if name == "" then
 		return
 	end
-
 	local n = tonumber(vim.fn.input("Number of arguments: "))
 	if n == nil or n < 0 or math.floor(n) ~= n then
 		print("Expected a non-negative integer")
 		return
 	end
-
 	local lines = {}
-
 	table.insert(lines, string.format("\tsubq (%d + 1)*8, %%SP, %%SP", n))
-
 	for i = 1, n do
 		table.insert(lines, string.format("\t// TODO: insert code to copy argument %d into %%2", i))
 		table.insert(lines, string.format("\tmovq %%2, 8*%d(%%SP)", i))
 	end
-
 	table.insert(lines, string.format("\tcall %s, %%2", name))
 	table.insert(lines, "\tmovq (%SP), %3   // return value now in %3")
 	table.insert(lines, string.format("\taddq (%d + 1)*8, %%SP, %%SP", n))
-
 	local row = vim.api.nvim_win_get_cursor(0)[1]
-
 	vim.api.nvim_buf_set_lines(0, row - 1, row - 1, false, lines)
-
-	-- Cursor auf das erste TODO setzen, falls es Argumente gibt
 	if n > 0 then
 		vim.api.nvim_win_set_cursor(0, { row + 1, 4 })
 	else
@@ -68,23 +54,19 @@ local function check_while_id(id)
 			return -1
 		end
 	end
-
 	return id
 end
 
 local function next_while_id()
 	local id = 0
-
 	while check_while_id(id) == -1 do
 		id = id + 1
 	end
-
 	return id
 end
 
 local function asm_while_loop()
 	local id = next_while_id()
-
 	local lines = {
 		string.format(".while%d:", id),
 		string.format("\t# insert here code for condition: If condition is false jump to .done.while%d", id),
@@ -93,17 +75,12 @@ local function asm_while_loop()
 		string.format("\tjmp .while%d", id),
 		string.format(".done.while%d:", id),
 	}
-
 	local row = vim.api.nvim_win_get_cursor(0)[1]
-
 	vim.api.nvim_buf_set_lines(0, row - 1, row - 1, false, lines)
-
-	-- Cursor in den Bedingungsblock setzen
 	vim.api.nvim_win_set_cursor(0, { row + 1, 8 })
 end
 
 vim.keymap.set("n", "<leader>a", "", { desc = "HPC0 Assembly" })
-
 vim.keymap.set("n", "<leader>af", asm_define_function, { desc = "Define function" })
 vim.keymap.set("n", "<leader>ac", asm_call_function, { desc = "Call function" })
 vim.keymap.set("n", "<leader>aw", asm_while_loop, { desc = "While loop" })
